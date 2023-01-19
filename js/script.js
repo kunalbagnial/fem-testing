d83cabcbf1ecdc4a02229eaa1e65b8657f8fe687
kunalbagnial <kunal.bagnial@gmail.com>
2023-01-19 16:27:28 +0530

    test commit

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
 const btnNavEl = document.querySelector(".header__toggle");
 const headerEl = document.querySelector(".header");
+const overlayEl = document.querySelector(".overlay");
 
 btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
+  if (headerEl.classList.contains("nav-open")) {
+    overlayEl.style.opacity = "1";
+  } else {
+    overlayEl.style.opacity = "0";
+  }
 });
